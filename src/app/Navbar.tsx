e81206156278423024ea8839bf7dfd09e2410e6c
HereisDisco <jmarroqu@uwo.ca>
2025-02-03 20:27:08 -0500

    Creating NavBar Page

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      {/* LOGO - Top Left */}
+      <Link href="/">
+        <span className={styles.logo}>LOGO</span>
+      </Link>
+
+      {/* Centered Navigation Links */}
+      <div className={styles.navlinks}>
+        <Link href="/features">
+          <span className={styles.navlink}>Features</span>
+        </Link>
+        <Link href="/about">
+          <span className={styles.navlink}>About Us</span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
