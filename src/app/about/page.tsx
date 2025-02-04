93baa958abca2e51d7158e2e835b55b24c417b95
HereisDisco <jmarroqu@uwo.ca>
2025-02-03 20:49:53 -0500

    Working Pages Blank

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutUs() {
+  return (
+    <main>
+      <h1>About Us Page</h1>
+      {/* More content here */}
+    </main>
+  );
+}
---
+++ src/app/features/page.tsx
@@ -0,0 +1,8 @@
+export default function Features() {
+  return (
+    <main>
+      <h1>Features Page</h1>
+      {/* More content here */}
+    </main>
+  );
+}
