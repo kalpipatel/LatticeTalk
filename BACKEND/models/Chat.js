c0efe8e1437159f483826279f51cea03543a4258
TejanveshG <gangavaraputejanvesh@gmail.com>
2025-03-06 22:21:02 -0500

    Updated Chat Schma

--- BACKEND/models/Chat.js
+++ BACKEND/models/Chat.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
+const MessageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Sender of the message
+  content: { type: String, required: true }, // Message content
+  timestamp: { type: Date, default: Date.now }, // Timestamp of when the message was sent
+});
+
 const ChatSchema = new mongoose.Schema({
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Users in chat
+  messages: [MessageSchema], // Store messages in order with sender and timestamp
   lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" }, // Last message in chat
+  User1KyberPub: { type: String }, // User1's Kyber public key
+  User2KyberPub: { type: String }, // User2's Kyber public key
+  User1SignPub: { type: String }, // User1's DSA public key
+  User2SignPub: { type: String }, // User2's DSA public key
   updatedAt: { type: Date, default: Date.now },
 });
 
